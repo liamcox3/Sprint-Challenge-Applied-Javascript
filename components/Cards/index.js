8ac14827bdd8f0e3bfddf70c9c2b1d48469c52de
Liam Cox <liamcox@Liams-MacBook-Pro.local>
2020-05-08 12:30:22 -0500

    finished

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,52 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+function cardCreator(data) {
+    let card = document.createElement("div");
+    let headline1 = document.createElement("div");
+    let author = document.createElement("div");
+    let imgContainer = document.createElement("div");
+    let img = document.createElement("img");
+    let span = document.createElement("span");
+
+    card.classList.add("card");
+    headline1.classList.add("headline");
+    author.classList.add("author");
+    imgContainer.classList.add("img-container");
+
+    card.appendChild(headline1);
+    card.appendChild(author);
+    author.appendChild(imgContainer);
+    imgContainer.appendChild(img);
+    card.appendChild(span);
+
+    // Text content
+    headline1.textContent = data.headline;
+    img.src = data.authorPhoto;
+    span.textContent = data.authorName;
+
+    return card;
+}
+
+cardContainer = document.querySelector(".cards-container");
+
+// Send an HTTP GET request to the following address: https://lambda-times-backend.herokuapp.com/articles
+// Study the response data you get back, closely.
+// You will be creating a card for each 'article' in the response.
+// This won't be as easy as just iterating over an array though.
+
+axios
+    .get("https://lambda-times-backend.herokuapp.com/articles")
+
+    .then((res) => {
+        console.log(res);
+        return res.data.articles;
+    })
+    .then((response) => {
+        Object.keys(response).forEach((art) => {
+            response[art].forEach((art1) => {
+                cardContainer.appendChild(cardCreator(art1));
+            });
+        });
+    });
--- components/Header/index.js
+++ components/Header/index.js
@@ -3,7 +3,7 @@
 // Write a function that returns the markup you see below:
 //
 //  <div class="header">
-//    <span class="date">SMARCH 28, 2019</span>
+//    <span class="date">MARCH 28, 2019</span>
 //    <h1>Lambda Times</h1>
 //    <span class="temp">98°</span>
 //  </div>
@@ -11,4 +11,26 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    let header = document.createElement("div");
+    let date = document.createElement("span");
+    let h1 = document.createElement("h1");
+    let temp = document.createElement("span");
+
+    header.classList.add("header");
+    date.classList.add("date");
+    header.classList.add("temp");
+
+    date.textContent = " MARCH 28, 2019";
+    h1.textContent = " Lambda Times";
+    temp.textContent = "98°";
+
+    header.appendChild(date);
+    header.appendChild(h1);
+    header.appendChild(temp);
+
+    return header;
+}
+
+let headerContainer = document.querySelector("div.header-container");
+headerContainer.appendChild(Header());
